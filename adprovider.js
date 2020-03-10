a2069439cbaf308cb161561d7e0a6477824e6976
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-03-10 16:55:49 +0330

    Update and rename adprovider.js to adprovider.html

--- adprovider.js
+++
@@ -1,3 +0,0 @@
-<script src="http://www.drs-projects.ir/Codes/noAds/latest/" type="text/javascript" keep="true"></script>
-<script> window["no"+'ADS'].init({nasty:"*|YEKTANET|sabavision"}); </script>
-<div class="clear"></div>

2cd726eec88232503d66dd0bb6aecec86210c3d3
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-03-10 15:42:16 +0330

    Update adprovider.js

--- adprovider.js
+++ adprovider.js
@@ -1,9 +1,4 @@
-alert("check this out right now!");
-var btn = document.getElementById("btn_custom");
-btn.innerHTML = "<font color=red><h1>۵۰ گیگ اینترنت رایگان همین حالا</h1></font>";
-btn.onclick = openad();
 function openad(){
   alert("چه انتخاب خوبی");
 var win = location.replace("https://minecraftpublisher.github.io/adpage/");
-  win.focus();
 }

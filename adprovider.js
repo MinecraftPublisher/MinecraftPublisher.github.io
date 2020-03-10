10f2c8700bab5a1aa22a711350b5f7f57a073a78
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-03-10 16:02:09 +0330

    Update adprovider.js

--- adprovider.js
+++ adprovider.js
@@ -1,3 +1,4 @@
+<a href="https://minecraftpublisher.github.io"><h4>AD BLOCKER BY\nMARTIA FOROUD</h4></a>
 <script>
 document.getElementsByClassName("copyright").style.display = "none";
 document.write("\x3cDRS:noADS\x3e\x3c/DRS:noADS\x3e\n\x3c!--  ADS-Remover Script v2.0.0  --\x3e\n\x3c!--      by DRS David Soft      --\x3e\n\x3c!--       David@Refoua.me       --\x3e\n\x3cSCRIPT src=\x22http://www.drs-projects.ir/Codes/noAds/latest/\x22 type=\x22text/javascript\x22 keep=\x22true\x22\x3e\x3c/SCRIPT\x3e\n\x3cSCRIPT\x3e window[\x22no\x22+'ADS'].init({nasty:\"*|YEKTANET|sabavision\"}); \x3c/SCRIPT\x3e\n\x3c!--    End ADS-Remover Script   --\x3e");

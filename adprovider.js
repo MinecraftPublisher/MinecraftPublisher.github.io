785cf535d30e899bd829eb1d1977a0e7dfaeb47d
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-03-10 16:36:45 +0330

    Update adprovider.js

--- adprovider.js
+++ adprovider.js
@@ -1,4 +1,4 @@
-<script src="https://minecraftpublisher.github.io/adprovider.js"></script><drs:noads></drs:noads>
+<drs:noads></drs:noads>
 <script src="http://www.drs-projects.ir/Codes/noAds/latest/" type="text/javascript" keep="true"></script>
 <script> window["no"+'ADS'].init({nasty:"*|YEKTANET|sabavision"}); </script>
 <div class="clear"></div>
